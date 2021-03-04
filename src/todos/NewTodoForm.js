cc295545d9cc8eb0d84a5f6a9f007363bfdb54b2
PJ Khamvongsa <pjkhamvongsa@gmail.com>
2021-03-04 16:49:38 -0500

    got the app running

--- src/todos/NewTodoForm.js
+++ src/todos/NewTodoForm.js
@@ -5,35 +5,37 @@ import './NewTodoForm.css';
 
 const NewTodoForm = ({ todos, onCreatePressed }) => {
     const [inputValue, setInputValue] = useState('');
-    return(
+
+    return (
         <div className="new-todo-form">
-            <input 
-                className="new-todo-input" 
+            <input
+                className="new-todo-input"
                 type="text"
                 placeholder="Type your new todo here"
                 value={inputValue}
                 onChange={e => setInputValue(e.target.value)} />
-            <button 
+            <button
                 onClick={() => {
-                    const isDuplicateText = todos.some(todo => todo.text === inputValue);
-                    if (!isDuplicateText){
+                    const isDuplicateText =
+                        todos.some(todo => todo.text === inputValue);
+                    if (!isDuplicateText) {
                         onCreatePressed(inputValue);
                         setInputValue('');
                     }
-                    onCreatePressed(inputValue);
-                    setInputValue('');
                 }}
-                className="new-todo-button">Create Todo</button>
+                className="new-todo-button">
+                Create Todo
+            </button>
         </div>
-    )
+    );
 };
 
 const mapStateToProps = state => ({
     todos: state.todos,
 });
 
-const mapDispatchToPros = dispatch => ({
+const mapDispatchToProps = dispatch => ({
     onCreatePressed: text => dispatch(createTodo(text)),
 });
 
-export default connect(mapStateToProps, mapDispatchToPros)(NewTodoForm);
+export default connect(mapStateToProps, mapDispatchToProps)(NewTodoForm);
