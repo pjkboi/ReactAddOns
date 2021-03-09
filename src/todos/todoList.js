7776a88586f2d5a00a7316056b24153d358c37a2
PJ Khamvongsa <pjkhamvongsa@gmail.com>
2021-03-09 13:51:02 -0500

    reverting the thunk test back

--- src/todos/todoList.js
+++ src/todos/todoList.js
@@ -6,13 +6,13 @@ import { removeTodo, markTodoAsCompleted } from './actions';
 import { displayAlert } from './thunks';
 import './TodoList.css';
 
-const TodoList = ({ todos = [], onRemovePressed, onCompletedPressed, onDisplayAlertClicked }) => (
+const TodoList = ({ todos = [], onRemovePressed, onCompletedPressed }) => (
     <div className="list-wrapper">
         <NewTodoForm />
         {todos.map(todo => <TodoListItem 
             todo={todo} 
             onRemovePressed={onRemovePressed} 
-            onCompletedPressed={onDisplayAlertClicked} />)}
+            onCompletedPressed={onCompletedPressed} />)}
     </div>
 );
 
@@ -23,7 +23,6 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => ({
     onRemovePressed: text => dispatch(removeTodo(text)),
     onCompletedPressed: text => dispatch(markTodoAsCompleted(text)),
-    onDisplayAlertClicked: text => dispatch(displayAlert(text)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
